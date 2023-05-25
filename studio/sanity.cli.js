48d160bc0e17dd91ad2e717e12d9e4008c0db5d3
Andreas Lundqvist <momentiris@gmail.com>
2023-05-25 22:57:26 +0200

    add email

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'r641vock',
-    dataset: 'ingridblix_dataset'
-  }
+    dataset: 'ingridblix_dataset',
+  },
 })
--- studio/schemas/siteSettings.js
+++ studio/schemas/siteSettings.js
@@ -15,11 +15,17 @@ export default {
       description: 'Describe your blog for search engines and social media.',
     },
     {
+      name: 'email',
+      type: 'email',
+      title: 'Email',
+      description: 'Email (shown on website)',
+    },
+    {
       name: 'keywords',
       type: 'array',
       title: 'Keywords',
       description: 'Add keywords that describes your blog.',
-      of: [{ type: 'string' }],
+      of: [{type: 'string'}],
       options: {
         layout: 'tags',
       },
